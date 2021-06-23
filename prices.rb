28946e64bd58fe2aefe641dab4731f4e66232337
Mauro Meden <mauro.meden@shopify.com>
2021-06-23 08:47:45 -0600

    Add answers

--- prices.rb
+++ prices.rb
@@ -34,5 +34,10 @@ puts do_something_with_every_item(prices, "show discounts")
 
 ### Assignment: answer the following questions:
 # 1. Which version of the code do you like better - the code in the last commit (page 165) or the previous one (page 164)? Why?
+#
+# A: I prefer the previous version (p. 164) because it was more readable and looked cleaber, even if there were some repetitions.
 
 # 2. Is there ever a good reason to repeat yourself in code (i.e. not have DRY code)? Give an example that justifies your answer.
+#
+# A: While there are multiple reasons for preferring DRY code, there are also cases in which it might not be necessary. A case that comes to mind is when two functions are very similar to each other, but they are not *fundamentally* the same.
+# For example, if I had to calculate the area of different geometric shapes, I could create a single function that covers all possibilities (for example by taking the shape name as a parameter), but it would be probably best to write different functions that are similar, but not the exactly the same. This would be very similar to the example provided in the book, where trying to abstract the code more actually made it uglier.
